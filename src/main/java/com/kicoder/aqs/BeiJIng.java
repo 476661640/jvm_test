7fb268573db8414d39817e7c6e220fdde4918594
wangnima <476661640@qq.com>
2018-08-30 17:11:25 +0800

    "三次：commit"

--- src/main/java/com/kicoder/aqs/BeiJIng.java
+++ src/main/java/com/kicoder/aqs/BeiJIng.java
@@ -3,5 +3,6 @@ package com.kicoder.aqs;
 public class BeiJIng {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("你好 再见");
     }
 }
